6e5ed8bb5f86e7881aa805167f951584f2c40951
Rabbi Hasan <jasrabbi50@gmail.com>
2024-09-18 13:36:11 +0600

    show seats page and booking seat view done

--- cinema/urls.py
+++ cinema/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('room/<int:room_id>/', views.room_movies, name='room_movies')
+    path('room/<int:room_id>/', views.room_movies, name='room_movies'),
+    path('movie/<int:movie_id>/seats/', views.movie_seats, name='movie_seats'),
+    path('movie/<int:movie_id>/seat/<int:seat_id>/book/', views.book_seat, name='book_seat'),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Room, Movie
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Room, Movie, Seat, Booking
 
 def index(request):
     rooms = Room.objects.all()
@@ -19,7 +19,30 @@ def room_movies(request, room_id):
     
     return render(request, 'room_movies.html', context)
 
-def movie_seats
+def movie_seats(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    seats = movie.room.seats.all()
+    seat_status = []
+    for seat in seats:
+        is_booked = Booking.objects.filter(movie=movie, seat=seat).exists()
+        seat_status.append({
+            'seat': seat,
+            'is_booked': is_booked
+        })
+
+    context = {
+        'movie': movie,
+        'seats': seat_status
+    }
+    return render(request, 'movie_seats.html', context)
+
+def book_seat(request, movie_id, seat_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    seat = get_object_or_404(Seat, id=seat_id)
+    if not Booking.objects.filter(movie=movie, seat=seat).exists():
+        Booking.objects.create(movie=movie, seat=seat)
+    return redirect('movie_seats', movie_id=movie.id)
+    
 
 
 
