756e142430cfe04834552f7edf75b192981ce36f
Rabbi Hasan <jasrabbi50@gmail.com>
2024-09-18 13:14:49 +0600

    create index page and room_service view

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import render, get_object_or_404
+from .models import Room, Movie
 
 def index(request):
     rooms = Room.objects.all()
@@ -8,3 +8,16 @@ def index(request):
     }
     
     return render(request, 'index.html', context)
+
+
+def room_movies(request, room_id):
+    room = get_object_or_404(Room.objects.prefetch_related('movies'), id=room_id)
+    context = {
+        'room': room,
+        'movies': room.movies.all()
+    }
+    
+    return render(request, 'room_movies.html', context)
+
+
+
