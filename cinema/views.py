01133b0717a082bcb8b773b092c13be9247aa680
Rabbi Hasan <jasrabbi50@gmail.com>
2024-09-18 13:05:14 +0600

    write index view for display all rooms

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Room
 
-# Create your views here.
+def index(request):
+    rooms = Room.objects.all()
+    context = {
+        'rooms': rooms
+    }
+    
+    return render(request, 'index.html', context)
